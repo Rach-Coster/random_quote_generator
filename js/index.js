8d432bc54a6abc7a13b3b9b3ea2c1d2f2bd66960
DESKTOP-1QRJ8SK\Rache <rachel.coster@outlook.com>
2017-10-23 21:53:48 +1100

    Added my css file because it wasn't there and fixed up some of my funky code

--- js/index.js
+++ js/index.js
@@ -1,57 +1,64 @@
-var colors = ['#0074D9', '#85144b', '#FF851B', '#FF4136'];
-var priorColor; 
+// Code here will be linted with JSHint.
+/* global $ */
+/* global window */ 
+/* global document */
+
+var colors = ["#0074D9", "#85144b", "#FF851B", "#FF4136"];
+var priorColor = ""; 
 var quote; 
 var author; 
 
-function changeColor(){
-   var randomColor = colors[Math.floor(Math.random() * colors.length)]; 
-  while(randomColor == priorColor){
-    randomColor = colors[Math.floor(Math.random() * colors.length)];
-  }
-  priorColor = randomColor;
-  $('#new-quote').css({backgroundColor: randomColor});
- $('body').animate({backgroundColor: randomColor});
+function randomColor(){ 
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    while(randomColor === priorColor){
+        randomColor = colors[Math.floor(Math.random() * colors.length)];
+    }
+    
+    priorColor = randomColor;
+    
+    return randomColor; 
 }
 
-function initColor(){
-   var randomColor = colors[Math.floor(Math.random() * colors.length)];
-  $('body').css({'background-color': randomColor});
-  $('#new-quote').css({'background-color': randomColor});
-  priorColor = randomColor; 
-  
+function changeColor(){
+    var newColor = randomColor(); 
+    $('#new-quote').css({backgroundColor: newColor});
+    $('body').animate({backgroundColor: newColor});
 }
 
 function getQuote() {
-  $.ajax({
-      url: "https://api.forismatic.com/api/1.0/?",
-      dataType: "jsonp",
-      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-      success: function( response ) {
-        $("#quote-text").html("<p id='quote-text'>" + "<i class='fa fa-quote-left'>" + "</i>" + "  " + response.quoteText + "</p>");
-        if(response.quoteAuthor == '') {
-          $("#quote-author").html("<p id = quote-author>" + " - Unknown" + "</p>")
-        }
+    $.ajax({
+        url: "https://api.forismatic.com/api/1.0/?",
+        dataType: "jsonp",
+        data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+        success: function( response ) {
+            $("#quote-text").html("<p id='quote-text'>" + "<i class='fa fa-quote-left'>" + "</i>" + "  " + response.quoteText + "</p>");
+            if(response.quoteAuthor === '') {
+                $("#quote-author").html("<p id = quote-author>" + " - Unknown" + "</p>");
+            }
         
-        else {
-            $("#quote-author").html("<p id = quote-author>" + "- " + response.quoteAuthor + "</p>")
-        }
+            else {
+                    $("#quote-author").html("<p id = quote-author>" + "- " + response.quoteAuthor + "</p>");
+               }
         
-        quote = response.quoteText; 
-        author = response.quoteAuthor; 
-      }
-  });
+            quote = response.quoteText; 
+            author = response.quoteAuthor; 
+        }
+    });
 }
 
 $(document).ready(function(){
-  initColor();
-  getQuote();
+    randomColor();
+    
+    changeColor();
+    getQuote();
 });
 
-$("#new-quote").click(function(){
-  changeColor();
-  getQuote();
+$(document).on('click', "#new-quote", function(){
+    changeColor();
+    getQuote();
 });
 
- $('#tweet').click(function(){
-   window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote +'"' + " " + author));
- });
+$(document).on('click', "#tweet", function(){
+    window.open('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '"' + " " + author));
+});
